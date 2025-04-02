e027e13d89253b825c11b48fcfbbf96f63c818af
유현호 <nolja126@gmail.com>
2025-04-02 20:46:30 +0900

    [Test] build test3

--- src/test/java/com/criminals/plusExponential/application/user/UserServiceTest.java
+++ src/test/java/com/criminals/plusExponential/application/user/UserServiceTest.java
@@ -17,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
-@TestPropertySource("classpath:properties/env.properties")
+@ActiveProfiles("test")
 @Transactional
 public class UserServiceTest {
 

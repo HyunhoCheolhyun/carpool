c8b077bdbbaa245ccada0d29dbf2de9aaa2329e1
유현호 <nolja126@gmail.com>
2025-04-02 20:40:42 +0900

    [Test] build test1

--- src/test/java/com/criminals/plusExponential/application/MatchedPathServiceTest.java
+++ src/test/java/com/criminals/plusExponential/application/MatchedPathServiceTest.java
@@ -5,10 +5,11 @@ import com.criminals.plusExponential.domain.embeddable.Coordinate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 public class MatchedPathServiceTest {
 
 
--- src/test/java/com/criminals/plusExponential/application/unmatchedPath/UnmatchedPathServiceTest.java
+++ src/test/java/com/criminals/plusExponential/application/unmatchedPath/UnmatchedPathServiceTest.java
@@ -12,10 +12,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 @Transactional
 @ExtendWith(MockitoExtension.class)
 public class UnmatchedPathServiceTest {

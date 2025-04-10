7b4ff7d2425ab8821d8ce80a64783bb1f9b8278b
baecheolhyeon <todd1130@naver.com>
2025-04-11 01:09:43 +0900

    [Feat] 매칭후 택시기사 화면 추가

--- src/main/java/com/criminals/plusExponential/presentation/PageController.java
+++ src/main/java/com/criminals/plusExponential/presentation/PageController.java
@@ -1,7 +1,9 @@
 package com.criminals.plusExponential.presentation;
 
+import com.criminals.plusExponential.domain.entity.MatchedPath;
 import com.criminals.plusExponential.domain.entity.Role;
 import com.criminals.plusExponential.infrastructure.config.security.CustomUserDetails;
+import com.criminals.plusExponential.infrastructure.persistence.MatchedPathRepository;
 import com.criminals.plusExponential.infrastructure.redis.RedisPgTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,8 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PageController {
 
     private final RedisPgTokenRepository redisPgTokenRepository;
+    private final MatchedPathRepository matchedPathRepository;
 
 
+    // 홈화면 -> 택시기사, 승객 따로 랜더링
     @GetMapping("/")
     public String home(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         if(customUserDetails.getUser().getRole() == Role.PASSENGER){
@@ -28,9 +32,24 @@ public class PageController {
         }
     }
 
+    // 결제완료 후 승객 화면
     @GetMapping("/matched")
     public String matched(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam(value = "pg_token") String pgToken) {
         redisPgTokenRepository.publishPaymentToken(customUserDetails.getUserId(),pgToken);
-        return "matched";
+        return "match-passenger";
+    }
+
+    // 매칭후 택시기사 화면
+    @GetMapping("/match-driver/{matchedPathId}")
+    public String matched(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("matchedPathId") Long matchedPathId) {
+        MatchedPath matchedPath = matchedPathRepository.findById(matchedPathId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 경로가 존재하지 않습니다: " + matchedPathId));
+
+        model.addAttribute("departure", "서울역");
+        model.addAttribute("waypoint1", "강남역");
+        model.addAttribute("waypoint2", "잠실역");
+        model.addAttribute("destination", "인천공항");
+
+        return "match-driver";
     }
 }
